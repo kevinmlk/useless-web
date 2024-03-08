bd630509bfa70071995c4aa07848f9cecec1c2fd
Kevin Malekera <r0900654@student.thomasmore.be>
2024-03-08 19:55:22 +0100

    Added a functionality that hides the intro section and shows the game section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,28 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '../particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+'use strict';
+
+// Setup
+const setup = () => {
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load('particles-js', '../particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+  // Btn's
+  const startBtn = document.querySelector('#start-button');
+
+  // Event listeners
+  startBtn.addEventListener('click', hideIntro);
+}
+
+// Hide intro section
+const hideIntro = () => {
+  // Quiz sections
+  const introSection = document.querySelector('#intro-section');
+  const quizSection = document.querySelector('#quiz-section');
+  
+  introSection.classList.add('hidden');
+  quizSection.classList.remove('hidden');
+}
+
+// Load setup when DOM content is loaded
+window.addEventListener("DOMContentLoaded", setup);
